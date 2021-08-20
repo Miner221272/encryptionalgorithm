47469ef57e8efd93fe4c941f215dc887133ef099
timothy bailey <52305261+Miner221272@users.noreply.github.com>
2021-08-20 14:25:08 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-test_str = "GeeksforGeeks"
+test_str = "a"
 res = ''.join(format(ord(i), '08b') for i in test_str)
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 digArr = []
@@ -12,24 +12,42 @@ for i in res:
     a.append(i)
 for i in a:
     digArr.append(int(i))
+print(digArr)
 print("starting to print the string in bianary")
 last = a[0]
 finalArray = []
 for i in digArr:
-    if i == last and index > 0:
-        if i == 0:
-            aCount += 1
-        if i == 1:
-            bCount += 1
+    if index > 0:
+        if i == last:
+            if i == 0:
+                aCount += 1
+            if i == 1:
+                bCount += 1
+        elif i is not last:
+            if i == 0:
+                aCount += 1     
+                finalArray.append(aCount)
+                aCount = 0
+            if i == 1:
+                bCount += 1
+                finalArray.append(alphabet[bCount])
+                bCount = 0
     elif index < 1:
         if i == 0:
             aCount += 1
         if i == 1:
             bCount += 1
-    elif i == 0:
-        finalArray.append(aCount)
-    elif i == 1:
-        finalArray.append(alphabet[index])
     index+=1    
     last = i
-print(bCount)
+if bCount > 0:
+    finalArray.append(aCount)
+if aCount > 0:
+    finalArray.append(alphabet[bCount])
+aCount = 0
+bCount = 0
+finalArray.pop(0)
+lastOne = []
+for i in finalArray:
+    if i is not 0:
+        lastOne.append(i)
+print(lastOne)
